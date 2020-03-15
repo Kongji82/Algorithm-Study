ef6d83af4dbd6b82aea6c027514d7a82aeb000e4
공지 <kongji4092@gmail.com>
2020-03-15 14:31:07 +0000

    last

--- Algorithm/11004.py
+++ Algorithm/11004.py
@@ -1,26 +1,5 @@
 # Boj 11004
+import sys
 
-def quick_seletion(arr, k):
-    pivot = arr[len(arr) + 1//2 - 1]
-    left, mid, right = [], [], []
-    
-    for i in range(len(arr)):
-        if arr[i] < pivot:
-            left.append(arr[i])
-        elif arr[i] > pivot:
-            right.append(arr[i])
-        else:
-            mid.append(arr[i])
-        
-    if k < len(left):
-        return quick_seletion(left, k)
-    elif k < len(left) + len(mid):
-        return mid[0]
-    else:
-        return quick_seletion(right, k - len(left) - len(mid))
-            
-
-N, K = map(int, input().split())
-num = list(map(int, input().split()))
-print(quick_seletion(num, K-1))
-
+N, K = map(int, sys.stdin.readline().split(' '))
+print(sorted(map(int, sys.stdin.readline().split(' ')))[K -1])
