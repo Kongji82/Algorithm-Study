ac3e1e9ee24a5c7485ffced12031affa4e2b3db0
공지 <kongji4092@gmail.com>
2020-03-15 14:15:00 +0000

    Second Try with Qucick Seletion

--- Algorithm/11004.py
+++ Algorithm/11004.py
@@ -1,20 +1,26 @@
 # Boj 11004
 
-def quick_sort(num):
-    if len(num) <= 1:
-        return num
-    pivot = num[len(num) // 2]
-    less_arr, equal_arr, big_arr = [], [], []
-    for i in num:
-        if i < pivot:
-            less_arr.append(i)
-        elif i > pivot:
-            big_arr.append(i)
+def quick_seletion(arr, k):
+    pivot = arr[len(arr) + 1//2 - 1]
+    left, mid, right = [], [], []
+    
+    for i in range(len(arr)):
+        if arr[i] < pivot:
+            left.append(arr[i])
+        elif arr[i] > pivot:
+            right.append(arr[i])
         else:
-            equal_arr.append(i)
-    return quick_sort(less_arr) + equal_arr + quick_sort(big_arr)
+            mid.append(arr[i])
+        
+    if k < len(left):
+        return quick_seletion(left, k)
+    elif k < len(left) + len(mid):
+        return mid[0]
+    else:
+        return quick_seletion(right, k - len(left) - len(mid))
+            
 
 N, K = map(int, input().split())
 num = list(map(int, input().split()))
-result = quick_sort(num)
-print(result[K-1])
+print(quick_seletion(num, K-1))
+
