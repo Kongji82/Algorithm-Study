7ba0b0d0fd8191a789c0e48ebf449882abc54a02
공지 <kongji4092@gmail.com>
2020-03-24 13:26:37 +0000

    Time over

--- Algorithm/11658.py
+++ Algorithm/11658.py
@@ -1,17 +1,22 @@
-N, M = map(int, input().split())
-field = []
-for _ in range(N):
-    field.append(list(map(int, input().split())))
+def query(field, x, y):
+    result = 0
+    for i in range(y):
+        for j in range(x):
+            result += field[i][j]
+            
+    return result
 
-for _ in range(M):
-    temp = list(map(int, input().split()))
-    if temp[0] == 0:
-        field[temp[1]-1][temp[2]-1] = temp[3]
-        print(field)
-    elif temp[0] == 1:
-        result = 0
-        for i in range(temp[1]-1 , temp[3]):
-            for j in range(temp[2]-1, temp[4]):
-                result += field[i][j]
-                    
-        print(result)
+# N, M = map(int, input().split())
+# field = []
+# for _ in range(N):
+#     field.append(list(map(int, input().split())))
+
+# for x in range(M):
+#     temp = list(map(int, input().split()))
+#     if temp[0] == 0:
+#         field[temp[2] - 1][temp[1] - 1] = temp[3]
+#     elif temp[0] == 1:
+#         result = query(field, temp[3], temp[4]) + query(field, temp[1], temp[2]) - query(field, temp[1], temp[4]) - query(field, temp[3], temp[2])
+#         print(result)
+field = [[1,2,3,4], [2,3,4,5]]
+print(query(field, 3,2))
