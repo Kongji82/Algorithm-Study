1ced9b7e2f4227d6048f84836c78ee14468a6d81
공지 <kongji4092@gmail.com>
2020-03-29 13:14:23 +0000

    Solve Runtime error

--- Algorithm/11726.py
+++ Algorithm/11726.py
@@ -1,7 +1,7 @@
 n = int(input())
 dp = [0] * (n+1)
-dp[1] = 1
-dp[2] = 2
-for i in range(3, n+1):
-    dp[i] = dp[i-1] + dp[i-2]
-print(dp)
+dp[0] = 1
+dp[1] = 2
+for i in range(2, n):
+    dp[i] = dp[i-2] + dp[i-1]
+print(dp[n-1] % 10007)
