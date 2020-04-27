3cb0173383a9b64ec17e3102ad111b8ac5afaa8b
공지 <kongji4092@gmail.com>
2020-04-27 13:54:05 +0000

    using fibonacci and memoization

--- Algorithm/1003.py
+++ Algorithm/1003.py
@@ -1,12 +1,24 @@
-fibo = [0] * 41
-fibo[0] = 0
-fibo[1] = 1
-for i in range(2, 41):
-    fibo[i] = fibo[i-1] + fibo[i-2]
+cache = [0] * 41
+cache[1] = 1
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif cache[n] != 0:
+        return cache[n]
+    else:
+        cache[n] =  fibonacci(n-1) + fibonacci(n-2)
+        return cache[n]
+
+N = int(input())
+
+for i in range(N):
+    num = int(input())
+    fibonacci(num)    
+    if num == 0:
+        print(1, 0)
+    else:
+        print(cache[num-1], cache[num])
     
-print(len(fibo))
-    
-# N = int(input())
-# for _ in range(N):
-#     num = int(input())
-#     fibonacci(num)
