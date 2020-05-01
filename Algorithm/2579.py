e75ebb94422b3495c8374400e214428d55c697a2
공지 <kongji4092@gmail.com>
2020-05-01 14:14:53 +0000

    using dp1

---
+++ Algorithm/2579.py
@@ -0,0 +1,13 @@
+N = int(input())
+score = [0 for i in range(301)]
+dp = [0 for i in range(301)]
+for i in range(N):
+    score[i] = int(input())
+
+dp[0] = score[0]
+dp[1] = score[0] + score[1]
+dp[2] = max(score[1] + score[2], score[0] + score[2])
+for i in range(3, N):
+    dp[i] = max(dp[i-2] + score[i], dp[i-3] + score[i-1] + score[i])
+    
+print(dp[N-1])
