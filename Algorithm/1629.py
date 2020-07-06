ef35c7dac1e58b09d7de547bf7d4e052db03afc0
공지 <kongji4092@gmail.com>
2020-07-06 13:25:05 +0000

    Divide Binary

---
+++ Algorithm/1629.py
@@ -0,0 +1,12 @@
+A, B, C = map(int, input().split())
+def squared(a, b, c):
+    if b == 1:
+        return a % c
+    else:
+        temp = squared(a, b//2, c)
+        if (b % 2 == 0):
+            return (temp * temp) % c
+        else:
+            return ((temp * temp)* a) % c
+    
+print(squared(A, B, C))
